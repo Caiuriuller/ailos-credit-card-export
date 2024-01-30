fc85bbefb6c290c3b120fdb1218c5275d78c537b
caiuri <caiuridh@gmail.com>
2024-01-30 00:42:01 -0300

    chore: comment remove

---
+++ ext.js
@@ -0,0 +1,5 @@
+//javascript:(function(){
+//    fetch('https://raw.githubusercontent.com/Caiuriuller/ailos-credit-card-export/main/index.js')
+//    .then(res => console.log(res.text().then(e => (eval(e)))))
+//    .catch((e) => console.log(e))
+//})();
--- index.js
+++ index.js
@@ -24,15 +24,3 @@
 
     prompt(`CTRL + C`, result.join(`\n`))
 })();
-
-
-//javascript:(function(){
-//    fetch('https://raw.githubusercontent.com/Caiuriuller/ailos-credit-card-export/main/index.js')
-//    .then(res => console.log(res.text().then(e => (eval(e)))))
-//    .catch((e) => console.log(e))
-//})();
-
-
-
-
-
